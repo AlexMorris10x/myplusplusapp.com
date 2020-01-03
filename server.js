1b54a0f8e0b7bc18e9f073fa8a516771ff479f0f
alexmorris10x <alexmorris10x@icloud.com>
2020-01-02 20:32:01 -0800

    .

--- server.js
+++ server.js
@@ -56,6 +56,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static( 'client/build' ));
+
+//   app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+//   });
+// }
+
 // ==== if its production environment!
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("/build"));
